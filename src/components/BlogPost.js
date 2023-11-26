e35871d3c7ead18f4c3867c890136631d2c9362f
Densel <densel.esekon@moringaschool.com>
2023-11-26 09:10:12 +0300

    Completed assignment

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BlogContent from "./BlogContent";
 import Comment from "./Comment";
+import Densel from "./Densel";
 
 function BlogPost() {
   return (
@@ -9,6 +10,7 @@ function BlogPost() {
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Densel comeHome="Densel welcome's you all to this blog, are you a big fun then subscribe."/>
     </div>
   );
 }
---
+++ src/components/Densel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Densel(props){
+    return(
+        <div className="densel">
+        {props.comeHome}
+        </div>
+    );
+}
+
+export default Densel;
